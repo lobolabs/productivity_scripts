08ce008474b5f3219e70b7a04e6d0d8470afbae0
abdullah samman <abdullah.samman@mokus.io>
2017-02-17 17:52:28 +0200

    add libs to workflow; add guarding to variables; add creds creation on first use(no terminal)

--- logtivly/credentials/credentials.py
+++ logtivly/credentials/credentials.py
@@ -10,10 +10,10 @@ from oauth2client.file import Storage
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Sheets API Python Quickstart'
-
+CREDS_FILENAME = 'sheets.googleapis.com-python-quickstart.json'
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -29,18 +29,14 @@ def get_credentials():
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-python-quickstart.json')
+                                   CREDS_FILENAME)
 
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
+        credentials = tools.run_flow(flow, store)
     return credentials
 
 def get_service_and_spreadsheetId():
